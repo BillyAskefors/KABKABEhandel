af8c13d01dab5f128b7a5babc6b0d9d9d6ef79e0
frlindqv <frlindqv@kth.se>
2016-03-09 16:26:23 +0100

    Method AddCustomer Updated

--- src/KABKABEhandel/Models/DAL/EHandelDB.cs
+++ src/KABKABEhandel/Models/DAL/EHandelDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,15 +11,28 @@ namespace KABKABEhandel.Models.DAL
     {
         const string ConnectionString = "Server=tcp:projekt2server.database.windows.net,1433;Database=Projekt 2 Databas;User ID=awa16@projekt2server;Password=AcademicWorkAcademy16;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
 
+        SqlCommand cmd; 
+
         public EHandelDB():base(new SqlConnection(ConnectionString))
         {
 
         }
 
-        public int AddCustomer(Customer newCustomer)
+        public int AddCustomer(Customer newCustomer, out string msg)
         {
-            return 1; 
+           
+            cmd = new SqlCommand();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "spAddCustomer"; 
+            cmd.Parameters.Add(new SqlParameter("@Email",SqlDbType.NVarChar,320)).Value = newCustomer.Email;
+            cmd.Parameters.Add(new SqlParameter("@FirstName", SqlDbType.NVarChar, 30)).Value = newCustomer.FirstName;
+            cmd.Parameters.Add(new SqlParameter("@LastName", SqlDbType.NVarChar, 30)).Value = newCustomer.LastName;
+            cmd.Parameters.Add(new SqlParameter("@Phone", SqlDbType.NVarChar, 30)).Value = newCustomer.Phone;
 
+            int affectedRows = ExcecuteNonQuery(cmd, out msg);
+
+            return affectedRows; 
+     
         }
 
         
