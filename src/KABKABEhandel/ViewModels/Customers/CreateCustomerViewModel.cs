e2ec0dcc939a84eb49575981b6b1a3a97736ffa6
Joachim Good <joachim.good@hotmail.com>
2016-03-09 14:15:04 +0100

    Change name from UserController to customer controller. Added viewmodels and views to CustomerController.

---
+++ src/KABKABEhandel/ViewModels/Customers/CreateCustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KABKABEhandel.ViewModels.Customers
+{
+    public class CreateCustomerViewModel
+    {
+    }
+}
