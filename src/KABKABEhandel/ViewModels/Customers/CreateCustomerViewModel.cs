754fca1b987b5c9798ed5df18b3dc13e005b3dc3
JonasKlintberg <github.com@czt.se>
2016-03-10 14:16:17 +0100

    Lagt till addressfält och "Accept terms" i Registrera kund.

--- src/KABKABEhandel/ViewModels/Customers/CreateCustomerViewModel.cs
+++ src/KABKABEhandel/ViewModels/Customers/CreateCustomerViewModel.cs
@@ -8,15 +8,37 @@ namespace KABKABEhandel.ViewModels.Customers
 {
     public class CreateCustomerViewModel
     {
-        [Required(ErrorMessage = "You must enter a firstname")]
+        [Display(Name = "Förnamn")]
+        [Required(ErrorMessage = "Du måste ange förnamn")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "You must enter a lastname")]
+        [Display(Name = "Efternamn")]
+        [Required(ErrorMessage = "Du måste anve efternamn")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "You must enter a phone number")]
+        [Display(Name = "Telefonnummer")]
+        [Required(ErrorMessage = "Du måste ange telefonnummer")]
         public string Phone { get; set; }
 
-     
+        [Display(Name = "Gatuadress")]
+        [Required(ErrorMessage = "Du måste ange gata och gatunummer")]
+        public string Street { get; set; }
+
+        [Display(Name = "Postnummer")]
+        [Required(ErrorMessage = "Du måste ange postnummer")]
+        public string Zip { get; set; }
+
+        [Display(Name = "Postort")]
+        [Required(ErrorMessage = "Du måste ange Postort")]
+        public string City { get; set; }
+
+        [Display(Name = "Land")]
+        [Required(ErrorMessage = "Du måste ange land")]
+        public string Country { get; set; }
+
+        [Display(Name = "Godkänn våra köpvillkor")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Du måste godkänna våra köpvillkor!")]
+        public bool AcceptTerms { get; set; }
+
     }
 }
