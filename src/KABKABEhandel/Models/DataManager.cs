95b5e74983c9153a1aba729d73796e33a1a6a9c1
Billy Askefors <b_askefors@hotmail.com>
2016-03-12 16:54:03 +0100

    Add product adden to product dropdown . Now linked to add product viewmodel.

--- src/KABKABEhandel/Models/DataManager.cs
+++ src/KABKABEhandel/Models/DataManager.cs
@@ -82,7 +82,7 @@ namespace KABKABEhandel.Models
         public ListProductViewModel[] GetLatestProducts()
         { 
             return db.GetLatestProducts()
-                .Select(product => new ListProductViewModel { ID = product.Id, Name = product.Name, Details = product.Description, Price = product.Price, Vat = product.Vat})
+                .Select(product => new ListProductViewModel { ID = product.Id, Name = product.Name, Details = product.Description, Price = product.Price, Vat = product.Vat, ImageURL = product.ImageURL})
                 .ToArray();
         }
 
